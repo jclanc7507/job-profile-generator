66fc279d3678af2adb461eb7562b28c2e5f21a93
Jeremiah <jclanc7507@gmail.com>
2022-03-07 19:23:08 -0500

    created Intern constructor with tdd, passes tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,4 +1,21 @@
-// port Employee
-// school 
-// getSchool()
-// getRole()[Intern]
+const Intern = require('../lib/Intern.js')
+
+test("creates Intern object", () => {
+    const intern = new Intern("Joshua Daniels", 345678, "joshjoshD@yahoo.com", "Hagerstown Community College")
+    expect(intern.name).toBe("Joshua Daniels")
+    expect(intern.id).toEqual(expect.any(Number))
+    expect(intern.email).toMatch("@")
+    expect(intern.school).toMatch("Hagerstown Community College")
+
+    console.log(Intern)
+})
+
+test ("fetches Intern's school name", () => {
+    const intern = new Intern("Joshua Daniels", 345678, "joshjoshD@yahoo.com", "Hagerstown Community College")
+    expect(intern.school).toMatch("Hagerstown Community College")
+})
+
+test("fetches Intern's role", () => {
+    const intern = new Intern("Joshua Daniels", 345678, "joshjoshD@yahoo.com", "Hagerstown Community College")
+    expect(intern.getRole()).toBe("Intern")
+})
